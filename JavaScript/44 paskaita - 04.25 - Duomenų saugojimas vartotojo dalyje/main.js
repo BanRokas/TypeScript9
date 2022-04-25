12dafd5928e98f9909abebfed195390563238d3b
BanRokas <rokas.banaitis@gmail.com>
2022-04-25 11:49:43 +0300

    44 paskaita - praktinė užduotis kartu

--- JavaScript/44 paskaita - 04.25 - Duomenų saugojimas vartotojo dalyje/main.js
+++ JavaScript/44 paskaita - 04.25 - Duomenų saugojimas vartotojo dalyje/main.js
@@ -23,3 +23,55 @@ for(let i = 0; i < sessionStorage.length; i++){
 sessionStorage.removeItem(sessionStorage.key(0)); // trina duomenį pagal raktinį žodį
 
 //            Užduotis kartu
+// localStorage.setItem("Vardas", "Rokas"); // sukuria
+// localStorage.setItem("Pavardė", "Banaitis");
+// localStorage.setItem("Amžius", "25");
+// localStorage.setItem("Lytis", "Vyras");
+document
+.querySelector("#formIrOutput > form")
+.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const key = e.target.elements.key.value;
+  const value = e.target.elements.value.value;
+
+  localStorage.setItem(key, value);
+
+  document.querySelector("#output > ul").remove();
+  uploadDataToScreen();
+
+  e.target.reset();
+});
+
+// function uploadDataToScreen(){}
+let uploadDataToScreen = () => {
+  const ul = document.createElement("ul");
+  for(let i = 0; i < localStorage.length; i++){
+    const li = document.createElement("li");
+
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+    const del = document.createElement('span');
+
+    del.textContent = "X";
+    // del.addEventListener('click', deleteLSE);
+    // del.id = key;
+    del.setAttribute("onclick", `deleteLSE('${key}')`);
+    del.classList.add('item');
+
+    li.textContent = `${key} - ${value}`;
+    li.appendChild(del);
+
+    ul.appendChild(li);
+  }
+  document.querySelector("#output").appendChild(ul);
+}
+
+// function deleteLSE(key){}
+let deleteLSE = key => {
+  localStorage.removeItem(key);
+  document.querySelector("#output > ul").remove();
+  uploadDataToScreen();
+} 
+
+uploadDataToScreen();
