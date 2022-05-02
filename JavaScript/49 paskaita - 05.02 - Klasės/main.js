838dffd63aa4b502c1f2e367cea52fcf4af1992a
BanRokas <rokas.banaitis@gmail.com>
2022-05-02 10:44:45 +0300

    49 paskaita - ats 3

--- JavaScript/49 paskaita - 05.02 - Klasės/main.js
+++ JavaScript/49 paskaita - 05.02 - Klasės/main.js
@@ -62,3 +62,27 @@ const manoKvadratas = new Rectangle(10, 12);
 console.log(manoKvadratas);
 console.log(manoKvadratas.area());
 console.log(manoKvadratas.perimeter());
+
+// 3
+class Point{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.coordinates = {x:this.x, y:this.y};
+  }
+  /*distance = function(kitasTaskas){
+
+  }*/
+  distance = kitasTaskas => {
+    // let a = this.x - kitasTaskas.x;
+    // let b = this.y - kitasTaskas.y;
+    // let atstumasTarpAIrB = Math.hypot(a, b).toFixed(2);
+    // return atstumasTarpAIrB;
+    return Math.hypot(this.x - kitasTaskas.x, this.y - kitasTaskas.y).toFixed(2);
+  }
+}
+const taskasA = new Point(0, 0);
+const taskasB = new Point(3, 4);
+console.log(taskasA);
+console.log(taskasB);
+console.log(`Atstumas tarp A ir B taškų yra ${taskasB.distance(taskasA)}.`);
